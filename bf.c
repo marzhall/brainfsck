2fd795d500695a86d85a0addfde0fb9f8cece0b6
Marshall Conover <marzhall.o@gmail.com>
2015-09-18 18:44:41 -0400

    count arguments so as not to segfault with no supplied program.

--- bf.c
+++ bf.c
@@ -5,6 +5,12 @@
 
 int main(int argc, char** argv)
 {
+    if (argc == 1)
+    {
+        printf("You need to pass the brainfuck program as an argument, e.g.\n ./bfi '++++++++++++++++++.'\n"); 
+        return 1;
+    }
+
     printf("\nRunning brainfuck interpretation. Output is:\n");
     interpereter(argv[1]);
     exit(0);
